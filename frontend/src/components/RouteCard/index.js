daac3ff7fcfc68b051ea1af1f40211e36b02c366
Zain Afzal <zainafzal52@gmail.com>
2022-11-16 18:09:56 -0500

    Made style change to card

--- frontend/src/components/RouteCard/index.js
+++ frontend/src/components/RouteCard/index.js
@@ -25,32 +25,38 @@ const RouteCard = ({ routeData, isFavorite }) => {
 
   return (
     <Card hoverable className="routecard_container">
-      <StarFilled
-        onClick={() => handleFavoriteClick()}
-        className={favorite ? "favorited_icon" : "non_favorited_icon"}
-      />
-      <Row justify="center">
-        <Row align="middle">
-          <Text>{`${origin} (${origin_iata}) `}</Text>
-          <CgArrowLongRight size={35} />
-        </Row>
-        <Col justify={"center"} align="middle">
-          <Text strong style={{ fontSize: 18 }}>
-            {airline}
-          </Text>
-          <Row justify="center" align="middle">
-            <IoIosAirplane size={35} />
+      <Col style={{ padding: 5 }} align="middle">
+        <StarFilled
+          onClick={() => handleFavoriteClick()}
+          className={favorite ? "favorited_icon" : "non_favorited_icon"}
+        />
+        <Text strong style={{ fontSize: 18 }}>
+          {airline}
+        </Text>
+        <Row style={{}} justify="center" align="middle">
+          <Row align="middle">
+            <Text
+              style={{ paddingRight: "15px" }}
+            >{`${origin} (${origin_iata}) `}</Text>
+            <CgArrowLongRight size={35} />
           </Row>
-
-          <Text style={{ display: "flex", height: "25px" }}>
-            {airplane_type}
-          </Text>
-        </Col>
-        <Row align="middle">
-          <CgArrowLongRight size={35} />
-          <Text>{`${destination} (${destination_iata}) `}</Text>
+          <Col justify={"center"} align="middle">
+            <Row
+              style={{ paddingLeft: "20px", paddingRight: "20px" }}
+              justify="center"
+              align="middle"
+            >
+              <IoIosAirplane size={35} />
+            </Row>
+          </Col>
+          <Row align="middle">
+            <CgArrowLongRight size={35} />
+            <Text
+              style={{ paddingLeft: "15px" }}
+            >{`${destination} (${destination_iata}) `}</Text>
+          </Row>
         </Row>
-      </Row>
+      </Col>
     </Card>
   );
 };
